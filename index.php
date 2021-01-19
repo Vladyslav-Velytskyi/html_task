ffc8ff935772211e5fef6c2d0991c690d8245ce0
Pavel <75396377+Konon-PV@users.noreply.github.com>
2021-01-19 02:16:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+<p><h2>Task 1</h2></p>
+<ol reversed>
+    <li class="with">Sunday</li>
+    <li class="with">Saturday</li>
+    <li class="with">Friday</li>
+    <li class="with">Thursday</li>
+    <li class="with">Wednesday</li>
+    <li class="with">Tuesday</li>
+    <li class="with">Monday</li>
+</ol>
+<p><h2>Task 2</h2></p>
+<table>
+    <tr>
+        <th colspan="2">Группа продуктов</th>
+        <th>Количество порций в день</th>
+        <th>Размер порции</th>
+        <th>Примеры продуктов</th>
+        <th>Значение</th>
+    </tr>
+    <tr>
+        <td class="first">Зерновые продукты</td>
+        <td class="image"><img src="img/bread.jpeg" alt=""></td>
+        <td>6-8</td>
+        <td>
+            <ul>
+                <li><i class="fa fa-check"></i>1 кусочек хлеба</li>
+                <li><i class="fa fa-check"></i>30г крупы или сухого завтрака</li>
+                <li><i class="fa fa-check"></i> &frac12 тарелки каши или макарон</li>
+            </ul>
+        </td>
+        <td>Хлеб (лучше цельнозерновой), пита, крупы, хлебцы, крекеры и т.п.</td>
+        <td>Главные источники энергии и пищевых волокон</td>
+    </tr>
+    <tr class="green">
+        <td class="first">Овощи</td>
+        <td class="image"><img src="img/vegetables.jpeg" alt=""></td>
+        <td>4-5</td>
+        <td>
+            <ul>
+                <li><i class="fa fa-check"></i>1 тарелка резаных листовых овощей</li>
+                <li><i class="fa fa-check"></i>&#189; тарелки<sup>*</sup> приготовленных овощей (варка, тушение и т.д.)</li>
+                <li><i class="fa fa-check"></i>170 мл овощного сока без сахара</li>
+            </ul>
+        </td>
+        <td>Помидоры, картофель, морковь, горошек, кабачки, любая капуста, шпинат, фасоль и т.д.</td>
+        <td>Богатые источники калия, магния и пищевых волокон</td>
+    </tr>
+    <tr>
+        <td class="first">Фрукты</td>
+        <td class="image"><img src="img/fruits.jpeg" alt=""></td>
+        <td>4-5</td>
+        <td>
+            <ul>
+                <li><i class="fa fa-check"></i>170 мл сока</li>
+                <li><i class="fa fa-check"></i>среднего размера фрукт</li>
+                <li><i class="fa fa-check"></i>&#189; чашки<sup>**</sup> свежих или замороженных фруктов, &#188;
+                    чашки сухофруктов</li>
+            </ul>
+        </td>
+        <td>Цитрусовые, виноград, бананы, абрикосы, персики, ягоды, финики, соки без сахара</td>
+        <td>Богатые источники калия, магния и пищевых волокон</td>
+    </tr>
+    <tr class="green">
+        <td class="first">Молочные продукты(обезжиренные или с низким содержанием жира)</td>
+        <td class="image"><img src="img/milf_product.jpeg" alt=""></td>
+        <td>2-3</td>
+        <td>
+            <ul>
+                <li><i class="fa fa-check"></i>220-230 мл молока</li>
+                <li><i class="fa fa-check"></i>1 чашка йогурта</li>
+                <li><i class="fa fa-check"></i>40-45 г сыра</li>
+            </ul>
+        </td>
+        <td>Молоко, кисломолочные продукты, творог, сыры - все обезжиренные или низкожирные (1%)</td>
+        <td>Богатые кальцием и белками</td>
+    </tr>
+    <tr>
+        <td class="first">Постное мясо, птица и рыба</td>
+        <td class="image"><img src="img/meat.jpeg" alt=""></td>
+        <td> < 170 г</td>
+        <td>
+            <ul>
+                <li><i class="fa fa-check"></i>80-90 г приготовленного мяса, птицы или рыбы</li>
+            </ul>
+        </td>
+        <td>Мясо постное, без видимого жира, птица без кожи. Рыба может быть жирной</td>
+        <td>Богаты белком и магнием</td>
+    </tr>
+    <tr class="green">
+        <td class="first">Орехи, семечки, сухие фасоль и горох</td>
+        <td class="image"><img src="img/nuts.jpeg" alt=""></td>
+        <td>4-5 в неделю</td>
+        <td>
+            <ul>
+                <li><i class="fa fa-check"></i>40-45 г орехов</li>
+                <li><i class="fa fa-check"></i>2 ст.л. семечек</li>
+                <li><i class="fa fa-check"></i>&#189; тарелки приготовленной фасоли или гороха</li>
+            </ul>
+        </td>
+        <td>Любые орехи, семечки подсолнечника или тыквы, фасоль, горох</td>
+        <td>Богатые энергией, магнием, калием, белками и волокнами</td>
+    </tr>
+    <tr>
+        <td class="first">Жиры и масла</td>
+        <td class="image"><img src="img/oil.jpeg" alt=""></td>
+        <td>2-3</td>
+        <td>
+            <ul>
+                <li><i class="fa fa-check"></i>1 ст.л. майонеза</li>
+                <li><i class="fa fa-check"></i>1 ч.л. растительного масла</li>
+                <li><i class="fa fa-check"></i>1 ч.л. спреда</li>
+                <li><i class="fa fa-check"></i>2 ст.л. салатного соуса</li>
+            </ul>
+        </td>
+        <td>Спреды(мягкие маргарины), низкожирный майонез, салатные соусы, любые растительные масла</td>
+        <td>Источник жирных кислот</td>
+    </tr>
+    <tr class="green">
+        <td class="first">Сладости</td>
+        <td class="image"><img src="img/choco.jpeg" alt=""></td>
+        <td>< 5 в неделю</td>
+        <td>
+            <ul>
+                <li><i class="fa fa-check"></i>1 ст.л. сахара</li>
+                <li><i class="fa fa-check"></i>1 ст.л. варенья</li>
+            </ul>
+        </td>
+        <td>Сахар, варенье, конфеты, зефир, пастила, мороженое</td>
+        <td>Сладости должны быть с низким содержанием жира</td>
+    </tr>
+</table>
+</body>
+</html>
